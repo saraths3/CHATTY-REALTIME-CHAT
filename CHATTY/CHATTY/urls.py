6e6e619234861aa1763f9fe4618d9b9adabbd4f3
sarath-s <sarath.work@zohomail.in>
2026-07-28 15:03:41 +0530

    Project Complete

--- CHATTY/CHATTY/urls.py
+++ CHATTY/CHATTY/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat',include('chat.urls') )
+    path('',include('chat.urls') )
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- CHATTY/chat/urls.py
+++ CHATTY/chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import chat
+from .views import chat, room
 
 urlpatterns = [
-    path('',  chat, name='index')
+    path('chat/',  chat, name='index'),
+    path('room/<str:room_name>/', room, name='room')
 ]
