80a68d41c042c341465030613a159b045af36d81
sarath-s <sarath.work@zohomail.in>
2026-07-29 13:49:54 +0530

    Models for the chat app, Room, Message

--- just_chat/chat/admin.py
+++ just_chat/chat/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Room, Message
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_filter = ('created_at','user')
+    readonly_fields = ('created_at',)
+    search_fields = ('name','user__username')
+    ordering = ('-created_at',)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_filter = ('created_at', 'user')
+    readonly_fields = ('created_at',)
+    search_fields = ('user__username', 'room__name')
+    ordering = ('-created_at',)
--- just_chat/chat/models.py
+++ just_chat/chat/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    passcode = models.CharField(max_length=16)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.user.username}'
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)  
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.room.name} - {self.content[:10]}'
